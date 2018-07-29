ad50f69b7b052acc3b9b47f48b5119b6a44a7ea9
Chad Reddick <chad_reddick2008@yahoo.com>
2018-07-29 16:17:51 -0700

    Added Weapon Class

---
+++ Public/Weapon.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Weapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WEAPONS_API AWeapon : public AItem
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
